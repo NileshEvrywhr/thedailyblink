1717b007e085d212fd0e5fa4cc7b0a54175070f4
NileshEvrywhr <nilesh.guptain@gmail.com>
2021-07-03 16:28:09 +0530

    add pdf,epub generation using metalsmith-ebook custom module

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const Metalsmith  = require('metalsmith');
 const markdown    = require('metalsmith-markdown');
 const layouts     = require('metalsmith-layouts');
-const pdf         = require('metalsmith-pdf');
+const makebook    = require('./custom_modules/metalsmith-ebook')
 
 Metalsmith(__dirname)
-  .source('/home/ubuntu/blinkist/en/daily/')
-  .destination('/home/ubuntu/legendary-literate/src/data/today/')
+  .metadata({
+    title: "file name"
+  })
+  .source('./src/blinkist/en/daily/')
+  .destination('./src/blinkist/en/daily/format/')
   .clean(true)
   .use(markdown())
   .use(layouts({
     engine: 'handlebars'
   }))
-  .use(pdf({
-    pattern: "**/*.html",
-    printMediaType: true,
-    pageSize: "letter"
+  .use(makebook({
+    title: "Book Title",
+    author: "Author Name",
+    pdf: {
+      "format": "letter",
+      "orientation": "portrait"
+    }
   }))
-  .build(function(err, files) {
+  .build(function(err) {
       if (err) {
-	console.log(err)
+	      console.log(err)
         throw err;
       }
   });
