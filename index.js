9752486f3f4ef5bacb328b4f2e7c71b9b728fe24
NileshEvrywhr <nilesh.guptain@gmail.com>
2021-07-03 17:26:44 +0530

    fix pdf encoding issue

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const Metalsmith  = require('metalsmith');
 const markdown    = require('metalsmith-markdown');
 const layouts     = require('metalsmith-layouts');
+const collections = require('metalsmith-collections');
 const makebook    = require('./custom_modules/metalsmith-ebook')
 
 Metalsmith(__dirname)
   .metadata({
-    title: "file name"
+    title: "File-Name"
   })
   .source('./src/blinkist/en/daily/')
   .destination('./src/blinkist/en/daily/format/')
   .clean(true)
+  .use(collections({
+    chapters: {
+      pattern: '*.md'
+    }
+  }))
   .use(markdown())
   .use(layouts({
     engine: 'handlebars'
